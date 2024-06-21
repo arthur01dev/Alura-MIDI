f0cca4d84bcfc2ab4754d91ace350b13b31dbcb1
arthur01dev <arthurrodriguesdesouza66@gmail.com>
2024-06-21 14:39:52 -0300

    adicionando algumas funcionalidades

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
-//  adicionarEventoTecla('.tecla_pom', '#som_tecla_pom');
-//  adicionarEventoTecla('.tecla_clap', '#som_tecla_clap');
-//  adicionarEventoTecla('.tecla_tim', '#som_tecla_tim');
-//  adicionarEventoTecla('.tecla_puff', '#som_tecla_puff');
-//  adicionarEventoTecla('.tecla_splash', '#som_tecla_splash');
-//  adicionarEventoTecla('.tecla_toim', '#som_tecla_toim');
-//  adicionarEventoTecla('.tecla_psh', '#som_tecla_psh');
-//  adicionarEventoTecla('.tecla_tic', '#som_tecla_tic');
-//  adicionarEventoTecla('.tecla_tom', '#som_tecla_tom');
-//  
-//  function tocarSom(som) {
-//      document.querySelector(som).play();
-//  }
-//  
-//  function adicionarEventoTecla(seletorTecla, seletorSom) {
-//      const tecla = document.querySelector(seletorTecla);
-//      tecla.addEventListener('click', () => tocarSom(seletorSom))
-//  }
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-listaDeTeclas[0].onclick = tocaSomPom;
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
 
-function tocaSomPom() {
-    document.querySelector('#som_tecla_pom').play();
+for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = () => {
+        tocaSom(idAudio);
+    };
+
+    tecla.onkeydown = () => {
+        tecla.classList.add('ativa')
+    }
+
 }
