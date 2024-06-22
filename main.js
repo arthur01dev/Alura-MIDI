f686e83e46b63b78eae501ced9b3adf4add9d857
Arthur Rodrigues de Souza <arthurrodriguesdesouza66@gmail.com>
2024-06-22 17:03:51 -0300

    finalizando o projeto
    Finalizando projeto Alura MIDI

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    elemento && elemento.localName === 'audio' ? elemento.play() : console.log('Elemento não encontrado ou seletor invalido.');
 }
 
-for(let contador = 0; contador < listaDeTeclas.length; contador++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
@@ -13,8 +15,17 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++) {
         tocaSom(idAudio);
     };
 
-    tecla.onkeydown = () => {
-        tecla.classList.add('ativa')
+    tecla.onkeydown = (evento) => {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = (evento) => {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.remove('ativa');
+        }
+
     }
 
 }
